26abf4a3b28e511cc87d099ee17c382fe005fccb
Mauro Amnirati <mauro.amnirati@gmail.com>
2025-02-01 14:14:16 -0300

    Creo la función para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
+let amigos = [];
+console.log(amigos);
+
+function agregarAmigo() {
+    let amigoAgregado = document.getElementById("amigo").value;
+
+    //Si el campo de texto está vacío
+        if (amigoAgregado == "") {
+            alert("Por favor, inserte un nombre.");
+        } else {
+            amigos.push(amigoAgregado);
+            console.log(amigos);
+        }
+       
+}
+
