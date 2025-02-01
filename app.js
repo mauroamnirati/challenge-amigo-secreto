e58090596c5065461ff649f4db593d8b797fe098
Mauro Amnirati <mauro.amnirati@gmail.com>
2025-02-01 15:41:02 -0300

    Creo e implemento la función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -11,10 +11,25 @@ function agregarAmigo() {
         } else {
             amigos.push(amigoAgregado);
             document.getElementById("amigo").value="";
+            actualizarListaAmigos();
             console.log(amigos);
         }
        
 }
+//Función para actualizar la lista de amigos en HTML
+function actualizarListaAmigos() {
+    //Obtener el elemento de la lista (ul)
+    let lista = document.getElementById("listaAmigos");
 
+    //Limpiamos la lista antes de agregar nuevos amigos
+    lista.innerHTML = "";
 
+    // Hacer un bucle for para recorrer el array de amigos
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li"); // Crear un nuevo <li>
+        li.textContent = amigos[i]; // Asignar el nombre del amigo al <li>
+        lista.appendChild(li); // Agregar el <li> a la lista
+    }
+
+}
 
